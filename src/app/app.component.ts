a9513288380326d0b98885f73c9c7580da3f5ed0
Robert EZIAN <robertkodjoezian@gmail.com>
2024-02-21 23:01:11 +0000

    second commit: creation des dossiers et fichier module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
+import { routes } from './app.routes';
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet, Routes } from '@angular/router';
+import { NavbarComponent } from "./sharepage/navbar/navbar.component";
+import { FooterComponent } from "./sharepage/footer/footer.component";
+import { HomeComponent } from './pages/home/home.component';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [CommonModule, RouterOutlet,RouterModule ,HomeComponent, NavbarComponent, FooterComponent]
 })
 export class AppComponent {
   title = 'website';
