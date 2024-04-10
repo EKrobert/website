88f544f40bcb4a3e1b4374c2016778c5f8b11646
Robert EZIAN <robertkodjoezian@gmail.com>
2024-04-10 23:14:18 +0000

    Ajout des images et détails des foods: creation de service/orderDetails

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { OrderDetailsService } from '../../services/order-details.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  constructor (private service:OrderDetailsService){}
+
+  foodData:any;
+
+  ngOnInit(): void {
+    this.foodData = this.service.foodDetails;
+  }
+
+
 
 }
